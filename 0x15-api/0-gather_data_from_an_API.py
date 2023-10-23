d475e37b0cdb8240a7cb9e689a1736c87b92c99d
vagrant <vagrant@linux-vag20>
2023-10-23 11:54:45 +0000

    Add task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# display Employee Tasks info
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get("{}users/{}".format(url, argv[1])).json()
+    todos = requests.get("{}users/{}/todos".format(url, argv[1])).json()
+
+    complete = []
+
+    for todo in todos:
+        if todo["completed"]:
+            complete.append(todo["title"])
+
+    print("Employee {} is done with tasks({}/{}):".format(user["name"], len(complete), len(todos)))
+    for item in complete:
+        print("\t{}".format(item))
