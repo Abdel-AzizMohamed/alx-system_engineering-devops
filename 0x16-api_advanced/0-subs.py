6c18e71ff72542d967db3e4f8b430e4327f4880d
Abdel-AzizMohamed <abdelazizmohamed7530@gmail.com>
2023-11-07 10:54:54 +0000

    Add task 1,2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,6 +5,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """returns the number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-agent': 'Mozilla/5.0'}
     req = requests.get(url, headers=headers, allow_redirects=False)
